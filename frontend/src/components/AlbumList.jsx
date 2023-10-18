00c90c3d04642084b4c0533b447be1bf16245be8
Khatantuul Batbold <batbold.k@northeastern.edu>
2023-10-18 13:09:54 -0400

    Styling changes to AlbumPreview component

--- frontend/src/components/AlbumList.jsx
+++ frontend/src/components/AlbumList.jsx
@@ -4,20 +4,33 @@ import { useUserContext } from './../context/usercontext';
 import axios from 'axios';
 
 
+
 const AlbumList = () => {
 
+  const componentStyle = {
+    display: 'flex',
+    gap: '20px',
+    border: '1px solid blue'
+    // fontSize: '16px',
+    // Add more styles as needed
+  };
+
   const {user} = useUserContext();
-  const [albums, setAlbums] = useState([]);
+  console.log('user here', user)
+  const [allAlbumsPopulated, setAllAlbumsPopulated] = useState([]);
 
 
   useEffect(() => {
     try{
       const response = axios.get(`http://localhost:9000/albums`,{
-        withCredentials: true
+        withCredentials: true,
+        headers: {
+          'preview': true
+        }
       })
       .then((res)=>{
         console.log('albums resp', res.data)
-        setAlbums(res.data);
+        setAllAlbumsPopulated(res.data);
       })
       .catch((err)=>{
         console.log('albums error',err)
@@ -28,9 +41,9 @@ const AlbumList = () => {
   },[])
 
   return (
-    <div className="album-list-wrapper">
-      {albums.map((album) => (
-        <AlbumPreview key={album._id} photos={album.photos} />
+    <div className="album-list-wrapper" style={componentStyle}>
+      {allAlbumsPopulated.map((album) => (
+        <AlbumPreview key={album.album._id} album={album.album} presignedUrls={album.presignedUrls} />
       ))}
     </div>
   );
--- frontend/src/components/AlbumPreview.jsx
+++ frontend/src/components/AlbumPreview.jsx
@@ -1,21 +1,23 @@
 import React from 'react'
 import './albumpreview.css';
 
-const AlbumPreview = ({photos}) => {
+const AlbumPreview = ({album,presignedUrls}) => {
+
+  
   return (
     <div class='album-preview-container'>
 
         {/* <div class='album-preview-wrapper'> */}
             <div className="preview-grid">
 
-            {photos.map((photo, index) => (
-        <img key={index} src={photo.url} alt={`Photo ${index + 1}`} />
+            {presignedUrls.map((presignedUrl, index) => (
+        <img key={index} src={presignedUrl.presignedUrl} alt={`Photo ${index + 1}`} />
       ))}
 
             </div>
         {/* </div> */}
             <div className="album-preview-title">
-                <span>Album Title</span>
+                <span>{album.title}</span>
             </div>
     </div>
     
