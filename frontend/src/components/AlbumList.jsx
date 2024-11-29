0e0ef51e321b0c4565c38bd2bdcdebe6fef76e2a
Khatantuul Batbold <batbold.k@northeastern.edu>
2024-11-28 21:30:17 -0500

    Album list and grid view changes

--- frontend/src/components/AlbumList.jsx
+++ frontend/src/components/AlbumList.jsx
@@ -1,74 +1,64 @@
-import React, { useEffect, useState } from 'react'
-import AlbumPreview from './AlbumPreview'
-import { useUserContext } from './../context/usercontext';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import AlbumPreview from "./AlbumPreview";
+import { useUserContext } from "./../context/usercontext";
+import axios from "axios";
 
-
-
-const AlbumList = ({onFetch}) => {
-
+const AlbumList = ({ onFetch }) => {
   const componentStyle = {
-    display: 'flex',
-    gap: '20px',
+    display: "flex",
+    gap: "30px",
+    "margin-top": "30px",
   };
 
-  const {user, updateUser, logoutUser} = useUserContext();
+  const { user, updateUser, logoutUser } = useUserContext();
   const [allAlbumsPopulated, setAllAlbumsPopulated] = useState([]);
-
 
   useEffect(() => {
     console.log("its working in albumslist");
-    try{
-      // const storedEtag = localStorage.getItem("albumsPreviewEtag") || '';
-      // console.log("storedEtag", storedEtag);
-      const response = axios.get(`http://localhost:9000/albums`,{
-        withCredentials: true,
-        headers: {
-          'preview': true,
-          // 'If-None-Match': storedEtag
-        }
-      })
-      .then((res)=>{
-        console.log("dsdae")
-      //  // Correctly retrieve the ETag
-      //  const newEtag = res.headers['etag']; // Access Axios headers
-      //  console.log("newEtag", newEtag);
- 
-      //  // Update localStorage with the new ETag
-      //  if (newEtag) {
-      //    localStorage.setItem("albumsPreviewEtag", newEtag);
-      //  }
-        setAllAlbumsPopulated(res.data);      
-        onFetch(res.data);
-       
-      })
-      .catch((err)=>{
-        if(err.status === 304){
-          console.log('Data served from browser cache');
-           // Fallback to cached state
-           if (allAlbumsPopulated && allAlbumsPopulated.length > 0) {
-            console.log("Using cached state");
-            setAllAlbumsPopulated(allAlbumsPopulated); // Reuse existing state
-          } else {
-            console.error("No cached data available in memory");
+    try {
+      const response = axios
+        .get(`http://localhost:9000/albums`, {
+          withCredentials: true,
+          headers: {
+            preview: true,
+            // 'If-None-Match': storedEtag
+          },
+        })
+        .then((res) => {
+          setAllAlbumsPopulated(res.data);
+          onFetch(res.data);
+        })
+        .catch((err) => {
+          if (err.status === 304) {
+            console.log("Data served from browser cache");
+
+            if (allAlbumsPopulated && allAlbumsPopulated.length > 0) {
+              console.log("Using cached state");
+              setAllAlbumsPopulated(allAlbumsPopulated);
+            } else {
+              console.error("No cached data available in memory");
+            }
           }
-        }
-        if(err.response.status === 401){
-          logoutUser();
-        }
-      })
-    }catch(err){
-      console.log('albumssss error',err)
+          if (err.response.status === 401) {
+            logoutUser();
+          }
+        });
+    } catch (err) {
+      console.log("albumssss error", err);
     }
-  },[])
+  }, []);
 
   return (
     <div className="album-list-wrapper" style={componentStyle}>
       {allAlbumsPopulated.map((album) => (
-        <AlbumPreview key={album.album._id} album={album.album} presignedUrls={album.presignedUrls} />
+        <AlbumPreview
+          key={album.album._id}
+          album={album.album}
+          presignedUrls={album.presignedUrls}
+        />
       ))}
     </div>
   );
 };
 
-export default AlbumList
+export default AlbumList;
