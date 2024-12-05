23f673aef4f630772377d2361d44bf108b31c3be
Khatantuul Batbold <batbold.k@northeastern.edu>
2024-12-04 22:00:25 -0500

    Debugging api base url

--- frontend/src/util/apiClient.jsx
+++ frontend/src/util/apiClient.jsx
@@ -4,4 +4,6 @@ const apiClient = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
+console.log(import.meta.env.VITE_API_BASE_URL); // Should log your backend URL
+
 export default apiClient;
