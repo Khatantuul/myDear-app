d92272398da6b3c02ae58d2e24db64186380cb7c
Khatantuul Batbold <batbold.k@northeastern.edu>
2024-12-04 20:41:54 -0500

    Fix in proxy in vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': import.meta.env.VITE_API_BASE_URL, 
+export default defineConfig(({ mode }) => {
+  // Load environment variables
+  const env = loadEnv(mode, process.cwd(), 'VITE_');
+
+  return {
+    plugins: [react()],
+    server: {
+      port: 3000,
+      proxy: {
+        '/api': env.VITE_API_BASE_URL || 'http://localhost:9000', 
+      },
     },
-  }
-})
+  };
+});
